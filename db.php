65bf7cc626a0ae91e4920af5a892645ea59b78cc
Carlos <saymonlenny@gmail.com>
2022-12-06 11:28:10 -0600

    d

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 //  creamos variables para conectarnos con la base de datos -->
 
-$host= "localhost";
+$host= "root";
 $bd= "db_registros"; //nombre de la base de datos
 $usuario="root";
 $contrasenia="";
