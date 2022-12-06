96b9aa6731614266f01cd6b7751f5a66ac335d11
Carlos <saymonlenny@gmail.com>
2022-12-06 11:35:17 -0600

    ss

--- db.php
+++ db.php
@@ -3,10 +3,11 @@
 
 $host= "localhost";
 $bd= "db_registros"; //nombre de la base de datos
-
+$usuario="root";
+$contrasenia="";
 
 try {
-  $conexion = new PDO("mysql:host=$host;dbname=$bd",$nombre, $apellido, $email); //PDO se utiliza para comunicar directamente con la base de datos
+  $conexion = new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia,$nombre, $apellido, $email); //PDO se utiliza para comunicar directamente con la base de datos
 
 } catch (Exception $ex) { //en caso si existe un error
   echo $ex->getMessage(); //imprima el error que se provoco
