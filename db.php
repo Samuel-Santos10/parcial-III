483d22e73ba08838b21826e41776f0d2e3d83d87
Carlos <saymonlenny@gmail.com>
2022-12-06 10:46:38 -0600

    conexion

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+//  creamos variables para conectarnos con la base de datos -->
+
+$host= "localhost";
+$bd= "db_registros"; //nombre de la base de datos
+$usuario="root";
+$contrasenia="";
+
+try {
+  $conexion = new PDO("mysql:host=$host;dbname=$bd",$nombre, $apellido, $email); //PDO se utiliza para comunicar directamente con la base de datos
+
+} catch (Exception $ex) { //en caso si existe un error
+  echo $ex->getMessage(); //imprima el error que se provoco
+}
+
+  ?>
